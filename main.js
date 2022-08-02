b2ba8ba01e58db9c2f198e1eed71fa43806e5f0f
tony2-5 <antdvory@gmail.com>
2022-08-01 20:36:44 -0400

    First Commit
    Made simple calculator layout, wrote super basic javascript math functions
    main.js not yet linked to index.html

---
+++ main.js
@@ -0,0 +1,12 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(...args) {
+    return num1 - num2;
+}
+function multiply(...args) {
+    return num1 * num2;
+}
+function divide(...args) {
+    return num1 / num2;
+}
