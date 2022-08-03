db378715b615d33bc1355f3323054f10cc2f87d9
tony2-5 <antdvory@gmail.com>
2022-08-03 17:14:13 -0400

    Finalized CSS & New JavaScript Feature
    Added custom font for entering numbers and result.
    You can now keep using operations without pressing equals and the answer will update

--- main.js
+++ main.js
@@ -1,25 +1,56 @@
 // array for storing two number values and operator
 var arr = [];
+var check = 0;
+const OPERATORS = "+-*/"
 // operation functions
 function add(num1, num2) {
-    arr.length = 0; // clears array
-    arr[arr.length] = num1 + num2; // sets first element of array to answer if user wants to keep using that value
-    return document.getElementById("result").innerHTML = num1 + num2;
+    // if user keeps using operators without pressing equals
+    if(arr.length > 3) {
+        arr.length = 0; // array wiped
+        check = 1; //set variable check to 1 to allow if statement to run inside of operation event switch statement
+        arr[arr.length] = num1 + num2; // first array element now answer
+        return document.getElementById("result").innerHTML = num1 + num2;
+    } else {
+        arr.length = 0; // clears array
+        arr[arr.length] = num1 + num2; // sets first element of array to answer if user wants to keep using that value
+        return document.getElementById("result").innerHTML = num1 + num2;
+    }
 }
 function subtract(num1, num2) {
-    arr.length = 0;
-    arr[arr.length] = num1 - num2;
-    return document.getElementById("result").innerHTML = num1 - num2;
+    if(arr.length > 3) {
+        arr.length = 0;
+        check = 1;
+        arr[arr.length] = num1 - num2;
+        return document.getElementById("result").innerHTML = num1 - num2;
+    } else {
+        arr.length = 0;
+        arr[arr.length] = num1 - num2;
+        return document.getElementById("result").innerHTML = num1 - num2;
+    }
 }
 function multiply(num1, num2) {
-    arr.length = 0;
-    arr[arr.length] = num1 * num2;
-    return document.getElementById("result").innerHTML = num1 * num2;
+    if(arr.length > 3) {
+        arr.length = 0;
+        check = 1;
+        arr[arr.length] = num1 * num2;
+        return document.getElementById("result").innerHTML = num1 * num2;
+    } else {
+        arr.length = 0;
+        arr[arr.length] = num1 * num2;
+        return document.getElementById("result").innerHTML = num1 * num2;
+    }
 }
 function divide(num1, num2) {
-    arr.length = 0;
-    arr[arr.length] = num1 / num2;
-    return document.getElementById("result").innerHTML = num1 / num2;
+    if(arr.length > 3) {
+        arr.length = 0;
+        check = 1;
+        arr[arr.length] = num1 / num2;
+        return document.getElementById("result").innerHTML = num1 / num2;
+    } else {
+        arr.length = 0;
+        arr[arr.length] = num1 / num2;
+        return document.getElementById("result").innerHTML = num1 / num2;
+    }
 }
 //clears calculator and array
 function clear() {
@@ -33,7 +64,7 @@ document.querySelector(".calcNumbers").addEventListener('click', (e) => {
         clear();
     }else {
         // concatenates user number input until and operator is selected
-        document.getElementById('numbers').innerHTML = document.getElementById('numbers').innerHTML.concat('', e.target.innerHTML);
+        if(document.getElementById('numbers').innerHTML.length < 16) document.getElementById('numbers').innerHTML = document.getElementById('numbers').innerHTML.concat('', e.target.innerHTML);
     }
 });
 
@@ -45,19 +76,42 @@ document.querySelector(".opButtons").addEventListener('click', (e) => {
     if(arr.length == 1 && document.getElementById('numbers').innerHTML.trim() != '') arr.length = 0; 
     // stores num1 and num2 into array, expects array element 1 and 3 to be numbers
     if(arr.length == 0 || arr.length == 2) arr[arr.length] = document.getElementById('numbers').innerHTML.trim();
-    //when users selects =, checks second array element expecting operation. If no operation sets number entered to answer
-    if(e.target.innerHTML == '=') {
+    //checks if user intents to make a bigger equation without pressing equals
+    // if the array length is equal to 3 and the next event taken is a operation allows for special case where array length is 4
+    if(arr.length == 3 && OPERATORS.includes(e.target.innerHTML)) {
+        arr[arr.length] = e.target.innerHTML;
+    }
+    // when users selects =, checks second array element expecting operation. If no operation sets number entered to answer
+    // arr length 4 is if user wants to keep using answer for operations without hitting equals
+    if(e.target.innerHTML == '=' || arr.length == 4) {
         switch(arr[1]) {
             case '+': add(parseInt(arr[0]), parseInt(arr[2]));
+            if(check == 1) {
+                arr[arr.length] = e.target.innerHTML;
+                check = 0;
+            }
             document.getElementById('numbers').innerHTML = "";
             break;
             case '-': subtract(parseInt(arr[0]), parseInt(arr[2]));
+            if(check == 1) {
+                // sets arr[1] to operation with arr[0] being the answer (no = needed)
+                arr[arr.length] = e.target.innerHTML;
+                check = 0;
+            }
             document.getElementById('numbers').innerHTML = "";
             break;
             case '*': multiply(parseInt(arr[0]), parseInt(arr[2]));
+            if(check == 1) {
+                arr[arr.length] = e.target.innerHTML;
+                check = 0;
+            }
             document.getElementById('numbers').innerHTML = "";
             break;
             case '/': divide(parseInt(arr[0]), parseInt(arr[2]));
+            if(check == 1) {
+                arr[arr.length] = e.target.innerHTML;
+                check = 0;
+            }
             document.getElementById('numbers').innerHTML = "";
             break;
             default: 
